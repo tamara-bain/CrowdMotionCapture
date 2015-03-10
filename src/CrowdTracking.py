650e349d0acb820144bbeb9e1c738825b6e8522e
Mike Feist <mdfeist@ualberta.ca>
2015-03-10 10:06:25 -0600

    Update drawing and track clean up.

--- src/CrowdTracking.py
+++ src/CrowdTracking.py
@@ -6,22 +6,22 @@ import sys
 help = 'Usage: python3 CrowdTracking.py <video file>'
 
 # Parameters
-maxCorners = 200
-qualityLevel = 0.1
-minDistance = 30
-blockSize = 16
+maxCorners = 100
+qualityLevel = 0.05
+minDistance = 50
+blockSize = 7
 
 # Track Buffer Length
-track_len = 20
+track_len = 60
 
 # Threshold
 threshold = 50
-distance = 5
-distance2 = 10
+distance = 2.5
+distance2 = 20
 
 # Find New Points
 newPoints_on = True
-tracks_on = True
+tracks_on = False
 
 def findGoodPoints(p0, p1, tracks, st):
     # Select good points
@@ -119,9 +119,6 @@ if __name__ == '__main__':
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     p0 = getNewPoints(frame_gray, old_gray, None)
 
-    # Create a mask image for drawing purposes
-    mask = np.zeros_like(old_frame)
-
     # Store tracks
     tracks = []
     tracks_index = []
@@ -145,6 +142,9 @@ if __name__ == '__main__':
                                                None, 
                                                **lk_params)
 
+        # Create a mask image for drawing purposes
+        mask = np.zeros_like(old_frame)
+
         if p1 != None:
             st = findGoodPoints(p0, p1, tracks, st)
 
@@ -156,9 +156,25 @@ if __name__ == '__main__':
                 a,b = new.ravel()
                 c,d = old.ravel()
                 cv = i % maxCorners
-                if tracks_on:
-                    mask = cv2.line(mask, (a,b),(c,d), color[cv].tolist(), 2)
                 frame = cv2.circle(frame,(a,b),5,color[cv].tolist(),-1)
+
+            if tracks_on and len(tracks) > 2:
+                upper = len(tracks)
+                lower = 0
+                if upper > 20:
+                    lower = upper - 20
+
+                number_of_points = len(tracks[-1])
+
+                for i in range(lower+1, upper):
+                    for j in range(number_of_points):
+                        if j >= len(tracks[i-1]):
+                            break
+
+                        a,b = tracks[i][j].ravel()
+                        c,d = tracks[i-1][j].ravel()
+                        cv = i % maxCorners
+                        mask = cv2.line(mask, (a,b),(c,d), color[cv].tolist(), 2)
 
         img = cv2.add(frame,mask)
 
@@ -213,6 +229,24 @@ if __name__ == '__main__':
                 while max(tracks_index) >= len(p0):
                     tracks_index.pop(-1)
 
+            # Split up tracks for drawing
+            if tracks_on:
+                for i in range(len(tracks)-1):
+                    n = len(tracks[i])
+                    new_points = np.zeros(n)
+                    for j,point in enumerate(tracks[i]):
+                        a,b = point.ravel()
+                        c,d = tracks[i+1][j].ravel()
+
+                        diff = getDistance(a, b, c, d)
+
+                        if diff > 20:
+                            new_points[j] = 1
+                            for k in range(i+1, len(tracks)):
+                                tracks[k] = np.concatenate((tracks[k], 
+                                    np.array([tracks[k][j]])))
+                                tracks[k][j] = point
+
         frame_count = frame_count+1
 
 
@@ -224,10 +258,15 @@ if __name__ == '__main__':
     print(len(tracks[-1]))
     mask2 = np.zeros_like(old_frame)
 
+    new_points = [0] * len(tracks[-1])
+
+    # Split tracks that move more than 20 pixels between frames
     for i in range(len(tracks)-1):
         n = len(tracks[i])
-        new_points = np.zeros(n)
         for j,point in enumerate(tracks[i]):
+            if new_points[j] == 1:
+                continue
+
             a,b = point.ravel()
             c,d = tracks[i+1][j].ravel()
 
@@ -235,14 +274,29 @@ if __name__ == '__main__':
 
             if diff > 20:
                 new_points[j] = 1
+                #new_points.append(0)
                 for k in range(i+1, len(tracks)):
-                    tracks[k] = np.concatenate((tracks[k], np.array([tracks[k][j]])))
+                    #tracks[k] = np.concatenate((tracks[k], np.array([tracks[k][j]])))
                     tracks[k][j] = point
 
+    print(len(tracks[-1]))
+
+    dead_points = [0] * len(tracks[-1])
+    dead_points_time = [0] * len(tracks[-1])
     for i in range(1,len(tracks)):
+        # Update time
+        for j in range(len(dead_points)):
+            if dead_points[j] == 1:
+                dead_points_time[j] = dead_points_time[j] + 1
+            else:
+                dead_points_time[j] = 0
+
         # Find points that don't move anymore
-        dead_points = np.zeros(len(tracks[i]))
+        # and mark as dead
         for j,point in enumerate(tracks[i]):
+            if dead_points[j] == 1:
+                continue
+
             a,b = point.ravel()
 
             dead_point = True
@@ -253,53 +307,18 @@ if __name__ == '__main__':
                 diff = getDistance(a, b, c, d)
 
                 if diff > distance/2.:
-                    print("Dead", j)
                     dead_point = False
                     break
 
             if dead_point:
                 dead_points[j] = 1
 
-        for j in range(len(dead_points)):
-            if dead_points[j] == 1:
-                a,b = tracks[i][j].ravel()
-                cv = j % maxCorners
-                mask2 = cv2.circle(mask2,(a,b),5,color[cv].tolist(),-1)
-
-#        shift = 0
-#        for j in range(len(tracks[i-1]), len(tracks[i])):
-#            index = j - shift
-#            a,b = tracks[i][j].ravel()
-#            for k in range(len(tracks[i-1])):
-#                if dead_points[k] == 1:
-#                    shift = shift + 1
-#                    c,d = tracks[i][k].ravel()
-#
-#                    diff = getDistance(a, b, c, d)
-#
-#                    if diff < distance:
-#                        for l in range(i+1, len(tracks)):
-#                            tracks[l][k] = tracks[l][j]
-#                            tracks[l] = np.delete(tracks[l], j, 0)
-#                        break
-
-    print(len(tracks[-1]))
-    
-#    for i in range(len(tracks)-1):
-#        n = len(tracks[i])
-#        new_points = np.zeros(n)
-#        for j,point in enumerate(tracks[i]):
-#            a,b = point.ravel()
-#            c,d = tracks[i+1][j].ravel()
-#
-#            diff = getDistance(a, b, c, d)
-#
-#            if diff > 20:
-#                new_points[j] = 1
-#                for k in range(i+1, len(tracks)):
-#                    tracks[k][j] = point
-#
-    print(len(tracks[-1]))
+#        # Draw Circles Where Dead
+#        for j in range(len(dead_points)):
+#            if dead_points[j] == 1:
+#                a,b = tracks[i][j].ravel()
+#                cv = j % maxCorners
+#                mask2 = cv2.circle(mask2,(a,b),5,color[cv].tolist(),-1)
 
     # Group Tracks
 
@@ -324,8 +343,27 @@ if __name__ == '__main__':
     img = cv2.add(np.uint8(0.5*old_frame), mask)
     img = cv2.add(img, mask2)
 
-    cv2.imshow('frame',img)
+    cv2.imshow('tracks',img)
     cv2.waitKey()
+
+    # Reset Video
+    cap.set(1, 0)
+
+    while(1):
+        ret, frame = cap.read()
+
+        if not ret:
+            break;
+
+        #img = cv2.add(frame,mask)
+
+        cv2.imshow('frame', frame)
+
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            exit()
+        if k == 10:
+            break
 
     cv2.destroyAllWindows()
     cap.release()
