2b6594b19e3de85058a58ed91b82ee300c45a752
Mike Feist <mdfeist@ualberta.ca>
2015-03-10 10:40:26 -0600

    Add comments to working code.

--- src/CrowdTracking.py
+++ src/CrowdTracking.py
@@ -22,6 +22,8 @@ distance2 = 20
 # Find New Points
 newPoints_on = True
 tracks_on = False
+draw_on = False
+
 
 def findGoodPoints(p0, p1, tracks, st):
     # Select good points
@@ -38,44 +40,73 @@ def findGoodPoints(p0, p1, tracks, st):
 
     return st
 
+
 def getNewPoints(img, img_old, points):
+    # Calculate the temporal difference between two frames
     t = np.int16(frame_gray) - np.int16(old_gray)
 
+    # If change is greater than threshold
+    # Then set value to 1
+    # Else set value to 0
     mask2 = abs(t) > threshold
+    # Cast mask to unsigned byte
     mask2 = mask2.astype('uint8')
 
-    # Find good features to detect
+    # Find good features to detect in areas of change
     p_tmp = cv2.goodFeaturesToTrack(old_gray, mask = mask2, **feature_params)
 
+    # If no points were given then return new points
     if points is None:
         return p_tmp
 
+    # If points were given and new points were found then merge
+    # the two sets of points
     if p_tmp != None:
         tmp = None
+
+        # Loop over new points
         for i,new in enumerate(p_tmp):
+
+            # Flag checking if new point is close to an
+            # existing point if so we don't add the new
+            # point
             found = False
+
+            # Loop over old points
             for j,old in enumerate(points):
                 a,b = new.ravel()
                 c,d = old.ravel()
 
+                # Calculate distance between points
                 diff = getDistance(a, b, c, d)
 
+                # If distance is less than threshold
+                # Then we don't add the new point
                 if (diff < distance):
                     found = True
                     break;
 
+            # If no existing point found then we add the new point
             if not found:
+                # If first new point then we save point
+                # Else we concatinate the new point with
+                # the other new points being added
                 if tmp == None:
                     tmp = np.array([np.copy(new)])
                 else:
                     tmp = np.concatenate((tmp, np.array([new])))
 
+        # If no new points found then no need to merge
         if tmp != None:
             if len(tmp) > 0:
+                # Merge the old points with the new points
                 points = np.concatenate((points, tmp))
 
+    # Return the updated points
     return points
 
+
+# Calculates the distance between two points
 def getDistance(a, b, c, d):
     dx = c-a
     dy = d-b
@@ -89,6 +120,8 @@ def getDistance(a, b, c, d):
 if __name__ == '__main__':
     print(help)
 
+    # Get video file if given
+    # Else open default camera
     if len(sys.argv) < 2:
         cap = cv2.VideoCapture(-1)
     else:
@@ -111,53 +144,70 @@ if __name__ == '__main__':
     # Create some random colors
     color = np.random.randint(0,255,(maxCorners,3))
 
-    # Take first frame and find corners in it
+    # Take first frame
     ret, old_frame = cap.read()
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 
+    # Take second frame
     ret, frame = cap.read()
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Find new points, the function uses the difference
+    # between the images to know where movement occured
     p0 = getNewPoints(frame_gray, old_gray, None)
 
     # Store tracks
     tracks = []
+    # Keep track of the index of each point
+    # This is needed because we are constantly adding
+    # and removing points
     tracks_index = []
 
+    # To keep track of the current frame
     frame_count = 0
+
     while(1):
+        # Grab new frame
         ret, frame = cap.read()
 
+        # Check if read was successful
         if not ret:
             break;
 
+        # Convert frame to gray scale
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        # Grab new points that are not being tacked
         if newPoints_on:
             p0 = getNewPoints(frame_gray, old_gray, p0)
 
-        # calculate optical flow
+        # Calculate optical flow and new location of points
         p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, 
                                                frame_gray, 
                                                p0, 
                                                None, 
                                                **lk_params)
 
-        # Create a mask image for drawing purposes
+        # Create a mask image for drawing tracks
         mask = np.zeros_like(old_frame)
 
         if p1 != None:
+            # Find points that don't move any more
             st = findGoodPoints(p0, p1, tracks, st)
 
+            # Remove invalid points (stationary)
             good_new = p1[st==1]
             good_old = p0[st==1]
 
-            # draw the tracks
-            for i,(new,old) in enumerate(zip(good_new,good_old)):
-                a,b = new.ravel()
-                c,d = old.ravel()
-                cv = i % maxCorners
-                frame = cv2.circle(frame,(a,b),5,color[cv].tolist(),-1)
+            # Draw the points
+            if draw_on:
+                for i,(new,old) in enumerate(zip(good_new,good_old)):
+                    a,b = new.ravel()
+                    c,d = old.ravel()
+                    cv = i % maxCorners
+                    frame = cv2.circle(frame,(a,b),5,color[cv].tolist(),-1)
 
+            # Draw tracks
             if tracks_on and len(tracks) > 2:
                 upper = len(tracks)
                 lower = 0
@@ -174,12 +224,22 @@ if __name__ == '__main__':
                         a,b = tracks[i][j].ravel()
                         c,d = tracks[i-1][j].ravel()
                         cv = i % maxCorners
-                        mask = cv2.line(mask, (a,b),(c,d), color[cv].tolist(), 2)
+                        mask = cv2.line(mask, 
+                                        (a,b),
+                                        (c,d), 
+                                        color[cv].tolist(), 
+                                        2)
 
+        # Combine current frame and mask 
+        # (draws points and tracks on frame)
         img = cv2.add(frame,mask)
 
+        # Show image
         cv2.imshow('frame',img)
 
+        # Handle for keyboard input
+        # ESC: Kills program
+        # Return: stops capturing frames and moves onto next step
         k = cv2.waitKey(30) & 0xff
         if k == 27:
             exit()
@@ -190,23 +250,42 @@ if __name__ == '__main__':
         old_gray = frame_gray.copy()
         p0 = good_new.reshape(-1,1,2)
 
-        # Find add new points to tracks
+        # Add new points to tracks
         if len(tracks) < 1:
             tracks.append(p0)
             tracks_index = [i for i in range(len(tracks))]
         else:
             # Find Valid Tracks for New Points
+
+            # Get number of tracks
             i = len(tracks)
+
+            # Duplicate last row of tracks
             tracks.append(tracks[i-1])
 
+            # Keeps track of the number of points removed
             removed = 0
+
+            # Loop through list of valid points this iteration
             for j in range(len(st)):
+
+                # Get the index of the points
                 index = j - removed
+
+                # Check if index is in tracks_index
                 if index not in tracks_index:
+                    # If not add it to the end
                     tracks_index.append(index)
 
+                # If point was removed due to being invalid
+                # Also check if index of point is already in
+                # tracks_index
                 if st[j] == 0 and j <= max(tracks_index):
+
+                    # Update removed count
                     removed = removed + 1
+
+                    # Try to get the index of the point in tracks
                     try:
                         index = tracks_index.index(j)
                     except ValueError:
@@ -214,17 +293,25 @@ if __name__ == '__main__':
                         print(tracks_index)
                         exit()
 
+                    # Update indices of points in tracks
                     tracks_index[index] = -1
                     for k in range(index+1,len(tracks_index)):
                         tracks_index[k] = tracks_index[k] - 1
 
+            # Loop through points and add them to tracks
             for j,new in enumerate(p0):
+                # Get index of point in tracks
                 index = tracks_index.index(j)
+
+                # If index found add point to track
+                # Else we add it to the end as it is a
+                # new track
                 if index < len(tracks[i]):
                     tracks[i][index] = new
                 else:
                     tracks[i] = np.concatenate((tracks[i], np.array([new])))
 
+            # Resize track_index
             if len(tracks[i]) < len(tracks_index):
                 while max(tracks_index) >= len(p0):
                     tracks_index.pop(-1)
@@ -247,6 +334,7 @@ if __name__ == '__main__':
                                     np.array([tracks[k][j]])))
                                 tracks[k][j] = point
 
+        # Update frame count
         frame_count = frame_count+1
 
 
@@ -274,7 +362,6 @@ if __name__ == '__main__':
 
             if diff > 20:
                 new_points[j] = 1
-                #new_points.append(0)
                 for k in range(i+1, len(tracks)):
                     #tracks[k] = np.concatenate((tracks[k], np.array([tracks[k][j]])))
                     tracks[k][j] = point
@@ -343,7 +430,7 @@ if __name__ == '__main__':
     img = cv2.add(np.uint8(0.5*old_frame), mask)
     img = cv2.add(img, mask2)
 
-    cv2.imshow('tracks',img)
+    cv2.imshow('tracks', img)
     cv2.waitKey()
 
     # Reset Video
