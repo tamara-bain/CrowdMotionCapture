6238a8998528f5483c61349702dada59f13e707d
Benjamin Lavin <blavin@ualberta.ca>
2015-04-01 15:56:45 -0600

    More unity with cylinder generation

--- Demo.cs
+++ Demo.cs
@@ -2,39 +2,53 @@
 using System.Collections;
 using System;
 using System.IO;
+using System.Collections.Generic;
 
-public class Demo : MonoBehaviour {	
+public class Demo : MonoBehaviour {
+	public List<GameObject> people = new List<GameObject>();
+	public List<String[]> tracks = new List<String[]> ();
+	Camera mainCam;
 	int x = 0;
 	int z = 0;
+	int count = 0;
 	string line;
 	char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
 	void Start(){
-
-
-		using (StreamReader reader = new StreamReader("file.txt"))
+		mainCam = Camera.main;
+		mainCam.transform.position = new Vector3 (0, 27, 0);
+		mainCam.transform.localEulerAngles = new Vector3 (15.5f, 0, 0);
+		mainCam.transform.localScale = new Vector3 (1, 1, 1);
+		using (StreamReader reader = new StreamReader("Assets/file.txt"))
 		{
-			line = reader.ReadLine();
+			do{
+				line = reader.ReadLine();
+				if (line != null){
+					people.Add(GameObject.CreatePrimitive(PrimitiveType.Cylinder));
+					string[] words = line.Split(delimiterChars);
+					tracks.Add(words);
+					count++;
+				}
+			} while (line != null);
 		}
-		Console.WriteLine(line);
-		string[] words = line.Split(delimiterChars);
-		print (words[1]);
-		x = Convert.ToInt32 (words[0]);
-		z = Convert.ToInt32 (words[1]);
-
 	}	
 	void Update(){
-
-		if(transform.position.x>x){
-			transform.position+= new Vector3 (-1, 0, 0);			
-		}
-		if(transform.position.x<x){
-			transform.position+= new Vector3 (1, 0, 0);			
-		}
-		if(transform.position.z>z){
-			transform.position+= new Vector3 (0, 0, -1);			
-		}
-		if(transform.position.z<z){
-			transform.position+= new Vector3 (0, 0, 1);			
+		for (int i = 0; i < count; i++) {
+			GameObject person = people[i] as GameObject;
+			//string[] personTrack = tracks[i] as string[];
+			x = Convert.ToInt32 (tracks[i][0]);
+			z = Convert.ToInt32 (tracks[i][1]);
+			if (person.transform.position.x > x) {
+				person.transform.position += new Vector3 (-1, 0, 0);			
+			}
+			if (person.transform.position.x < x) {
+				person.transform.position += new Vector3 (1, 0, 0);			
+			}
+			if (person.transform.position.z > z) {
+				person.transform.position += new Vector3 (0, 0, -1);			
+			}
+			if (person.transform.position.z < z) {
+				person.transform.position += new Vector3 (0, 0, 1);			
+			}
 		}
 	}
 }
